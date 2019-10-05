25e58d945cd719740f2206d493ad8d87710ecac1
Flore <Flore@DESKTOP-RC1ET5L>
2019-10-05 17:37:56 -0500

    définir position finale

--- MazePart1/src/mazepart1/Form.java
+++ MazePart1/src/mazepart1/Form.java
@@ -18,74 +18,81 @@ import javax.swing.JFrame;
  *
  * @author cesar
  */
-public class Form extends JFrame{
+public class Form extends JFrame {
+
     private static final long seialVersionUID = 1L;
-    private static final int BRICK_SIZE=20;
+    private static final int BRICK_SIZE = 20;
     public int posXPerso;
     public int posYPerso;
+    public int posXFinal;
+    public int posYFinal;
+
     ArrayList<String> bricks;
-    
-    
-    
-    public Form(ArrayList<String> bricks,int posXPerso, int posYPerso){
-        if(!bricks.isEmpty()){
-            int width=bricks.get(0).toCharArray().length*BRICK_SIZE;
-            int height=bricks.size()*BRICK_SIZE;
-            this.setSize(width+(3*BRICK_SIZE),height+(3*BRICK_SIZE));
+
+    public Form(ArrayList<String> bricks, int posXPerso, int posYPerso, int posXFinal, int posYFinal) {
+        if (!bricks.isEmpty()) {
+            int width = bricks.get(0).toCharArray().length * BRICK_SIZE;
+            int height = bricks.size() * BRICK_SIZE;
+            this.setSize(width + (3 * BRICK_SIZE), height + (3 * BRICK_SIZE));
         }
-        
-        this.bricks=bricks;
-        this.posXPerso=posXPerso;
-        this.posYPerso=posYPerso;
+
+        this.bricks = bricks;
+        this.posXPerso = posXPerso;
+        this.posYPerso = posYPerso;
+        this.posXFinal = posXFinal;
+        this.posYFinal = posYFinal;
+
     }
-    
+
     @Override
     public void paint(Graphics g) {
         super.paintComponents(g);
-        
+
         Graphics2D painter = (Graphics2D) g.create();
         painter.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        
+
         painter.setColor(Color.WHITE);
         painter.fillRect(0, 0, getWidth(), getHeight());
-        
+
         Color line = Color.GRAY;
         Color muros = Color.LIGHT_GRAY;
         Color fill = Color.BLUE;
         Color text = Color.BLACK;
         Color perso = Color.RED;
-        
-        char c=1; 
-        int i=1;
+
+        char c = 1;
+        int i = 1;
         int x = 0;
-        int y = BRICK_SIZE+20;
-        
-        for (String wall : this.bricks){  
-            
+        int y = BRICK_SIZE + 20;
+
+        for (String wall : this.bricks) {
+
             StringTokenizer st = new StringTokenizer(wall, ",");
             painter.setColor(perso);
             painter.fillOval(posXPerso, posYPerso, 14, 14);
-            x = BRICK_SIZE+10;
-            
-            while (st.hasMoreTokens()){
+            char [] carray = { 'F'};
+            painter.drawChars(carray, 0, 1, posXFinal, posYFinal);
+            x = BRICK_SIZE + 10;
+
+            while (st.hasMoreTokens()) {
                 String nombre = st.nextToken();
-                if (nombre.equals("#")){
+                if (nombre.equals("#")) {
                     painter.setColor(fill);
                     painter.fillRect(x, y, BRICK_SIZE, BRICK_SIZE);
                     painter.setColor(line);
                     painter.drawRect(x, y, BRICK_SIZE, BRICK_SIZE);
 
-                }else if(nombre.equals(".")){
+                } else if (nombre.equals(".")) {
                     painter.setColor(line);
                     painter.drawRect(x, y, BRICK_SIZE, BRICK_SIZE);
-               
-                } else if (nombre.equals("A")) {                       
+
+                } else if (nombre.equals("A")) {
                     painter.setColor(text);
                     String stringValueOf = String.valueOf((char) (c + 'A' - 1));
                     System.out.println(stringValueOf);
                     painter.drawRect(x, y, BRICK_SIZE, BRICK_SIZE);
                     painter.drawString(stringValueOf, x + 7, y + 14);
-                    c+=1;
+                    c += 1;
 
                 } else if (nombre.equals("B")) {
                     System.out.println(i);
@@ -93,25 +100,27 @@ public class Form extends JFrame{
                     painter.drawRect(x, y, BRICK_SIZE, BRICK_SIZE);
                     painter.drawString(Integer.toString(i), x + 7, y + 14);
                     i += 1;
-                    
-                } else if (nombre.equals("0")) {  
+
+                } else if (nombre.equals("0")) {
                     painter.setColor(muros);
                     painter.fillRect(x, y, BRICK_SIZE, BRICK_SIZE);
                     painter.setColor(line);
                     painter.drawRect(x, y, BRICK_SIZE, BRICK_SIZE);
 
-                } else if (nombre.equals("Z")) {                       
+                } else if (nombre.equals("Z")) {
                     painter.setColor(text);
                     painter.drawRect(x, y, BRICK_SIZE, BRICK_SIZE);
 
                 }
                 x += BRICK_SIZE;
-                
+
             }
             y += BRICK_SIZE;
         }
+        
     }
-    
+ 
+
     @Override
     protected void processKeyEvent(KeyEvent ke) {
 
@@ -119,19 +128,19 @@ public class Form extends JFrame{
             return;
         }
         if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
-            if (posXPerso + 20 < 13*BRICK_SIZE) {
-                posXPerso  += 20;
+            if (posXPerso + 20 < 13 * BRICK_SIZE) {
+                posXPerso += 20;
             }
         } else if (ke.getKeyCode() == KeyEvent.VK_LEFT) {
-            if (posXPerso  - 40 > 2*BRICK_SIZE) {
-                posXPerso  -= 20;
+            if (posXPerso - 40 > 2 * BRICK_SIZE) {
+                posXPerso -= 20;
             }
         } else if (ke.getKeyCode() == KeyEvent.VK_UP) {
-            if (posYPerso  - 40 > 3*BRICK_SIZE) {
+            if (posYPerso - 40 > 3 * BRICK_SIZE) {
                 posYPerso -= 20;
             }
         } else if (ke.getKeyCode() == KeyEvent.VK_DOWN) {
-            if (posYPerso + 20 < 14*BRICK_SIZE) {
+            if (posYPerso + 20 < 14 * BRICK_SIZE) {
                 posYPerso += 20;
             }
         }
--- MazePart1/src/mazepart1/MazePart1.java
+++ MazePart1/src/mazepart1/MazePart1.java
@@ -25,39 +25,39 @@ public class MazePart1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-                String firstNumber = JOptionPane.showInputDialog("Input Horizontal (A-J) :");
+        String firstNumber = JOptionPane.showInputDialog("Input Horizontal (A-J) :");
         String secondNumber = JOptionPane.showInputDialog("Input Vertical (1-10) :");
         int num1 = 0;
         switch (firstNumber) {
             case "A":
-                num1 = 53;
-                break;
-            case "B":
                 num1 = 73;
                 break;
-            case "C":
+            case "B":
                 num1 = 93;
                 break;
-            case "D":
+            case "C":
                 num1 = 113;
                 break;
-            case "E":
+            case "D":
                 num1 = 133;
                 break;
-            case "F":
+            case "E":
                 num1 = 153;
                 break;
-            case "G":
+            case "F":
                 num1 = 173;
                 break;
-            case "H":
+            case "G":
                 num1 = 193;
                 break;
-            case "I":
+            case "H":
                 num1 = 213;
                 break;
-            case "J":
+            case "I":
                 num1 = 233;
+                break;
+            case "J":
+                num1 = 253;
                 break;
 
             default:
@@ -66,34 +66,34 @@ public class MazePart1 {
         int num2 = Integer.parseInt(secondNumber);
         switch (num2) {
             case 1:
-                num2 = 63;
-                break;
-            case 2:
                 num2 = 83;
                 break;
-            case 3:
+            case 2:
                 num2 = 103;
                 break;
-            case 4:
+            case 3:
                 num2 = 123;
                 break;
-            case 5:
+            case 4:
                 num2 = 143;
                 break;
-            case 6:
+            case 5:
                 num2 = 163;
                 break;
-            case 7:
+            case 6:
                 num2 = 183;
                 break;
-            case 8:
+            case 7:
                 num2 = 203;
                 break;
-            case 9:
+            case 8:
                 num2 = 223;
                 break;
-            case 10:
+            case 9:
                 num2 = 243;
+                break;
+            case 10:
+                num2 = 263;
                 break;
 
             default:
@@ -102,46 +102,122 @@ public class MazePart1 {
         String sum = firstNumber + secondNumber;
         JOptionPane.showMessageDialog(null, "Position is : " + sum, "Initial position", JOptionPane.PLAIN_MESSAGE);
 
-        ArrayList<String> bricks=MazePart1.read("data/MazeMap.txt");
-        
-        Form window = new Form(bricks,num1+20,num2+20);
+        String posxfinal = JOptionPane.showInputDialog("Input Horizontal (A-J) :");
+        String posyfinal = JOptionPane.showInputDialog("Input Vertical (1-10) :");
+        int num3 = 0;
+        switch (posxfinal) {
+            case "A":
+                num3 = 73;
+                break;
+            case "B":
+                num3 = 93;
+                break;
+            case "C":
+                num3 = 113;
+                break;
+            case "D":
+                num3 = 133;
+                break;
+            case "E":
+                num3 = 153;
+                break;
+            case "F":
+                num3 = 173;
+                break;
+            case "G":
+                num3 = 193;
+                break;
+            case "H":
+                num3 = 213;
+                break;
+            case "I":
+                num3 = 233;
+                break;
+            case "J":
+                num3 = 253;
+                break;
+
+            default:
+            // code block
+        }
+        int num4 = Integer.parseInt(posyfinal);
+        switch (num4) {
+            case 1:
+                num4 = 83;
+                break;
+            case 2:
+                num4 = 103;
+                break;
+            case 3:
+                num4 = 123;
+                break;
+            case 4:
+                num4 = 143;
+                break;
+            case 5:
+                num4 = 163;
+                break;
+            case 6:
+                num4 = 183;
+                break;
+            case 7:
+                num4 = 203;
+                break;
+            case 8:
+                num4 = 223;
+                break;
+            case 9:
+                num4 = 243;
+                break;
+            case 10:
+                num4 = 263;
+                break;
+
+            default:
+            // code block
+        }
+        String sum2 = posxfinal + posyfinal;
+        JOptionPane.showMessageDialog(null, "Final Position is : " + sum2, "Initial position", JOptionPane.PLAIN_MESSAGE);
+        ArrayList<String> bricks = MazePart1.read("data/MazeMap.txt");
+
+        Form window = new Form(bricks, num1, num2, num3+3,num4+12);
         window.setTitle("Maze");
         window.setLocationRelativeTo(null);
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
     }
-    
+
     private static ArrayList<String> read(String filename) {
         ArrayList<String> lines = new ArrayList<>();
-        try{
+        try {
             FileInputStream fstream = new FileInputStream(filename);
             DataInputStream in = new DataInputStream(fstream);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String line;
-            
+
             while ((line = br.readLine()) != null) {
-                if (line.length()!=26){
+                if (line.length() != 26) {
                     lines.add(line);
-                }else{
+                } else {
                     System.err.println("Incorect lenght in the .txt file");
                     System.err.println(line.length());
                     System.exit(0);
                 }
-                
+
             }
-            
-            in.close();        
-        }catch (Exception e){
+
+            in.close();
+        } catch (Exception e) {
             System.err.println("Error :" + e.getMessage());
         }
-        
-        if (lines.size()!=13){
+
+        if (lines.size() != 13) {
             System.err.println("Incorrect number of lines in the .txt file");
             System.exit(0);
         }
         return lines;
-                
+
     }
-    
+
 }
